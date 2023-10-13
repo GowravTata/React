6498b2d9e1b306f022bca5b9165d865caa887202
GowravTata <gowravsaitata@gmail.com>
2023-10-13 16:41:11 +0530

    Added code upto conditional rendering

--- 2-pizza-menu/src/index.js
+++ 2-pizza-menu/src/index.js
@@ -76,39 +76,71 @@ function Header() {
 }
 
 function Menu() {
+  // const pizzas = [];
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+  // console.log(numPizzas);
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      {
+        // In the below we checked if there are pizzas , display onlt if the pizzas exist
+        numPizzas > 0 ? (< ul className="pizzas">
+          {pizzas.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
+        </ul>) :
+          <p>
+            We're Still working on our menu. Please come back later 😊
+          </p>
+      }
+      {/* Render the below only if we have pizzas */}
+
       {/* Each component can be used multiple times */}
-      <Pizza
+      {/* <Pizza
         name='Pizza Spinaci'
         ingredients='Tomato, mozarella, spinach, and ricotta cheese'
         photoName='pizzas/spinaci.jpg'
-        price={10} />  {/* props were written like this */}
-    </main>
+        price={10} />  props were written like this */}
+
+    </main >
   )
 }
 
 function Pizza(props) {
   // Passing the data from parent to child, i.e., is from Menu to Pizza
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   )
 }
 
 function Footer() {
   const hour = new Date().getHours();
   const [openHour, closeHour] = [12, 22];
-  const isOpened = hour >= openHour && hour <= closeHour ? 'open' : 'closed'
+  // const isOpen = hour >= openHour && hour <= closeHour ? 'open' : 'closed';
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
-    <footer className="footer">{new Date().toLocaleTimeString()} We're Currently {isOpened}</footer>
+    <footer className="footer">
+      {/* {new Date().toLocaleTimeString()} We're Currently {isOpen} */}
+      {/* Conditional Rendering to display if the shop is opened based on the condition
+      this is done by short circuiting */}
+      {isOpen ?
+        (
+          <div className="order">
+            <p>We're open until {closeHour}:00. Come visit us or order online.
+            </p>
+            <button className="btn">Order</button>
+          </div>
+        ) :
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>}
+    </footer>
   );
   // return React.createElement('footer', null, "We're currently open")
 }
