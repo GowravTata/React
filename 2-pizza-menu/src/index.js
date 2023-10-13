a0978d8ea0ebd7b20c4d97430b4d9b09ccee1b9d
GowravTata <gowravsaitata@gmail.com>
2023-10-13 18:06:04 +0530

    Adding commit before coding challenge

--- 2-pizza-menu/src/index.js
+++ 2-pizza-menu/src/index.js
@@ -85,9 +85,19 @@ function Menu() {
       <h2>Our Menu</h2>
       {
         // In the below we checked if there are pizzas , display onlt if the pizzas exist
-        numPizzas > 0 ? (< ul className="pizzas">
-          {pizzas.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
-        </ul>) :
+        numPizzas > 0 ? (
+          // The below is called a react fragment
+          // For rendering a list we need to use a different format
+          <React.Fragment>  {/*A key also can be added to it */}
+            <p>
+              Authentic Italian cuisine, 6 creative didhes to choose from. All from
+              our stone oven, all organic, all declicious.
+            </p>
+            < ul className="pizzas">
+              {pizzas.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
+            </ul>
+          </React.Fragment>
+        ) :
           <p>
             We're Still working on our menu. Please come back later 😊
           </p>
@@ -105,17 +115,22 @@ function Menu() {
   )
 }
 
-function Pizza(props) {
-  // Passing the data from parent to child, i.e., is from Menu to Pizza
+function Pizza({ pizzaObj }) {
+  // Passing the data from parent to child, i.e., is from Menu to Pizza 
+  // if (props.pizzaObj.soldOut) return null;
+  // console.log(pizzaObj);
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    // Used template string below to grey the area, wherever the pizza is sold out 
+    // Used the conditional string to set the class name
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""} `} >
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        {pizzaObj.soldOut ? <span>SOLD OUT</span> : <span>{pizzaObj.price}</span>}
+        {/* <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span> */}
       </div>
-    </li>
+    </li >
   )
 }
 
@@ -124,26 +139,39 @@ function Footer() {
   const [openHour, closeHour] = [12, 22];
   // const isOpen = hour >= openHour && hour <= closeHour ? 'open' : 'closed';
   const isOpen = hour >= openHour && hour <= closeHour;
+  if (!isOpen) return (
+    <p>
+      We're happy to welcome you between {openHour}:00 and {closeHour}:00
+    </p >)
   return (
     <footer className="footer">
       {/* {new Date().toLocaleTimeString()} We're Currently {isOpen} */}
       {/* Conditional Rendering to display if the shop is opened based on the condition
       this is done by short circuiting */}
       {isOpen ?
-        (
-          <div className="order">
-            <p>We're open until {closeHour}:00. Come visit us or order online.
-            </p>
-            <button className="btn">Order</button>
-          </div>
-        ) :
+        <Order closeHour={closeHour} openHour={openHour} /> :
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>}
-    </footer>
+    </footer >
   );
   // return React.createElement('footer', null, "We're currently open")
 }
+
+// Extracting JSX into a new component
+// If we try to destructure the property that doesn't exist ,it will not receive anything
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  )
+}
+
+
 // Creating a new component
 /*
 Function should start with Capital Letter
