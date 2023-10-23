d9f003930d8e6613f4e0f8b03a3de2d9654d3a37
GowravTata <gowravsaitata@gmail.com>
2023-10-23 09:42:14 +0530

    Code before adding sorting

--- 10-travel-list-state/src/App.js
+++ 10-travel-list-state/src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -17,12 +17,24 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -52,7 +64,7 @@ function Form({ onAddItems }) {
       package: false,
       id: Date.now(),
     };
-    console.log(newItem);
+    // console.log(newItem);
     // Adding the items to the
     onAddItems(newItem);
     // After the submission, the form should go to the initial state, it can be done like this
@@ -85,13 +97,18 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {/* Using Items state directly in the JSX */}
         {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
         {/* LIST */}
       </ul>
@@ -99,22 +116,44 @@ function PackingList({ items, onDeleteItem }) {
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}{" "}
       </span>
-      <button onClick={onDeleteItem}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
       {/* <button onClick={() => alert("Deleted")}>❌</button> */}
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  console.log(`Number of items  are ${numItems}`);
+  console.log(`Percentage of items packed are ${percentage}`);
+  console.log(`Number of items packed are ${numPacked}`);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, you already packed X ( X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything to go! Ready to go 🛫"
+          : `💼 You have ${numItems} items on your list, you already packed
+        ${numPacked} ${percentage}%`}
+      </em>
     </footer>
   );
 }
