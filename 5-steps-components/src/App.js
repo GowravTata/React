f0f8ef0c46c79332aaa6dbc4a68f9c6687d6df32
GowravTata <gowravsaitata@gmail.com>
2023-10-25 17:32:10 +0530

    Added New Project;

--- 5-steps-components/src/App.js
+++ 5-steps-components/src/App.js
@@ -14,6 +14,14 @@ export default function App() {
     return (
         <div>
             <Steps />
+            <StepMessage step={1}>
+                <p>Pass in Content</p>
+                <p>✌🏻</p>
+            </StepMessage>
+            <StepMessage step={2}>
+                <p>Read children pop</p>
+                <p>😎</p>
+            </StepMessage>
         </div>
     )
 }
@@ -58,10 +66,16 @@ function Steps() {
                     </div>
 
                     {/* In react, we use HTML event listener, write on the place where they will happen */}
-                    <p className="message">Step {step}: {messages[step - 1]}
+                    {/* <p className="message">
+                        <h3>Step {step}</h3> {messages[step - 1]}
 
                         {/* BAD PRACTICE  {test.name} */}
-                    </p>
+                    {/* </p> */}
+                    <StepMessage step={step}>{messages[step - 1]}
+                        <div className="buttons">
+                            <Button bgColor='#e7e7e7' textColor='#333' onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>Learn How</Button>
+                        </div>
+                    </StepMessage>
                     <div className="buttons">
                         {/* In React, we add the events directly on the event handlers */}
                         <Button textColor={textColor} bgColor={bgColor} onClick={handlePrevious}>
@@ -75,11 +89,18 @@ function Steps() {
     );
 }
 
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3>
+            {children}
+        </div>)
+}
+
 function Button({ textColor, bgColor, onClick, children }) {
     return (
         <button style={{ backgroundColor: bgColor, color: textColor }}
             onClick={onClick}>
-            {/* <span>{emoji}</span> */}
             {children}
         </button>
     )
